34a6938caba87ef2bb97875e241411674713713a
Eduard Aleksandrov <eduard.ale@list.ru>
2023-03-21 18:46:08 +0300

    поиск второго положительного fix

--- b0301/lec21-home.asm
+++ b0301/lec21-home.asm
@@ -1,3 +1,5 @@
+; Нахождение второго четного элемента, ноль является четным
+
 DSEG SEGMENT PARA PUBLIC 'DATA'
     array dw 3,1,1,3,4,5,6,7
     b dw 1b
@@ -12,7 +14,7 @@ SSTACK ENDS
 
 CSEG SEGMENT PUBLIC 'CODE'
     ASSUME CS:CSEG, DS:DSEG, SS:SSTACK
-print_str proc ; результат
+print_str proc ; печать результата
     mov ah, 2h
     mov dx, bx
     add dx, '0'
@@ -54,26 +56,5 @@ exit:
     mov AL, 0
     mov AH, 4ch
     int 21h
-; START PROC FAR
-;     mov ax, DSEG
-;     mov ds, ax
-;     m1: mov k, 2
-;         mov si, 0
-;         mov cx, 8
-;         mov al, b
-;     m2: TEST x[si], al
-;         JNZ m3
-;         DEC k
-;         JZ m4
-;     m3: inc si
-;         loop m2
-;     m4: add si, '0'
-;         mov ah, 2h
-;         mov dx, si
-;     m5: int 21h
-;     m6: mov ah, 4ch
-;         mov al, 0
-;         int 21h
-; START ENDP
 CSEG ENDS
     END START
