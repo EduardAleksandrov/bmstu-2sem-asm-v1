0afc312d55116c936669b20f567ada65a930b335
Eduard Aleksandrov <eduard.ale@list.ru>
2023-05-03 23:08:35 +0300

    матрица с лабораторной

---
+++ b0501/factlec.asm
@@ -0,0 +1,52 @@
+SS1 Segment para stack 'stack'
+	dw 64 dup(0)
+SS1 ends
+
+sd1 Segment para 'data'
+	n0 dw 4
+	f dw ?
+	str db 'start$'
+sd1 ends
+
+sc1 Segment para 'code'
+	assume cs:sc1, ds:sd1, ss:ss1
+
+fact proc near
+	push bp
+	mov bp, sp
+	n equ [bp+4]
+	mov cx, n
+	dec cx
+	jne m1
+	mov ax, 1
+	pop bp
+	ret
+	m1: 
+		push cx
+		call fact
+		add sp, 2
+		mov cx, n
+		mul cx
+		pop bp
+		ret
+fact endp
+start proc
+	mov ax, sd1
+	mov ds, ax
+	push n0
+	call fact
+	pop bx
+	mov f, ax
+	
+	mov dx, offset str
+	mov ah, 09h
+	int 21h
+	
+	
+	
+	mov ah, 4ch
+	int 21h
+start endp
+sc1 ends
+	end start
+	
---
+++ b0501/matrix.asm
@@ -0,0 +1,58 @@
+; транспонирование матрицы, из лекции (сделано на лабораторной) - работает
+DSEG Segment
+    nmax dw 4
+    n dw 4
+    x db '1234'
+      db '1234'
+      db '1234'
+      db '1234'
+DSEG ENDS
+
+CSEG Segment
+ASSUME CS:CSEG, DS:DSEG, SS:SSEG
+START:
+    mov ax, DSEG
+    mov ds, ax
+
+    mov bx, 1
+    mov dx, nmax
+    mov cx, n
+    dec cx
+    m1: 
+        push cx
+        mov si, bx
+        mov di, dx
+    m2:
+        mov al, x[si]
+        xchg al, x[di]
+        xchg al, x[si]
+        add di, nmax
+        inc si
+        loop m2
+        add dx, nmax
+        inc dx
+        add bx, nmax
+        inc bx
+        pop cx
+        loop m1
+
+
+    ; добавляем доллар в конец строки
+    mov si, 16
+    mov cl, '$'
+    mov BX, OFFSET x
+    mov [BX+SI], cl
+    ; выводим строку
+    mov AH, 09h
+    mov DX, OFFSET x
+    int 21h
+
+
+    mov ah, 4ch
+    int 21h
+CSEG ENDS
+
+SSEG Segment Stack
+    dw 64 dup(0)
+SSEG ENDS
+    END START
